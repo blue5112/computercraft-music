ccd671a53beed024ae807b8790275c03873049f5
blue5112 <reliant511@yahoo.com>
2023-04-21 20:51:59 -0700

    Add files via upload

---
+++ playall.lua
@@ -0,0 +1,17 @@
+args = {...}
+shuffle = require("./lib/shuffle")
+playfile = require("./lib/playfile")
+location = "/songs/"
+
+if not args[1] then args[1] = 0.2 end
+args[1] = tonumber(args[1])
+
+local list = fs.list("songs")
+
+for n=1, #list, 1 do
+    --print(list[n])
+    shell.run("clear")
+    print("Playing " .. list[n])
+    list[n] = location .. list[n]
+    playfile.play_dfpwm(list[n],args[1])
+end
